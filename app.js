703b7d6962a2a80dc48a125ce8d2307457bf6b27
Darryl Kilzer <darrylkilzer@gmail.com>
2017-04-26 15:46:31 -0600

    fix alignment

--- app.js
+++ app.js
@@ -17,18 +17,17 @@ function ItunesController() {
     for (var i = 0; i < songList.length; i++) {
       var song = songList[i]
       template += `
-    <div class="col-md-5 col-md-offset-1">
+    <div class="col-md-4 col-md-offset-1">
       <div class="panel panel-default">
         <div class="panel-heading"
           <h4 class="panel-title"><strong>${song.artist}</strong></h4>
         </div>
         <div class="panel-body">
           <img height="100px" width="100px" style="center" src="${song.albumArt}">
-          <ul style="list-style: none">
-          <li><strong>Collection: ${song.collection}</strong></li>
-          <li><strong>Price: $${song.price}</strong></li>
-          <li><strong>Song Title: ${song.title}</strong></h4>
-          <li><audio id="audio" controls><source src="${song.preview}" type="audio/ogg"></audio> </li>
+          <p><strong>Collection: ${song.collection}</strong></p>
+          <p><strong>Price: $${song.price}</strong></p>
+          <p><strong>Song Title: ${song.title}</strong></p>
+          <p><audio id="audio" controls><source src="${song.preview}" type="audio/ogg"></audio> </p>
         </div>
       </div>
     </div>
